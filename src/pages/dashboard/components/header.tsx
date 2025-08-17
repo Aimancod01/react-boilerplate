ba2cc0e2b1a2ff6309a16d61739e6395630eb067
Aiman Naeem <109482563+Aimancod01@users.noreply.github.com>
2025-08-17 22:06:58 +0500

    logout handler

--- src/pages/dashboard/components/header.tsx
+++ src/pages/dashboard/components/header.tsx
@@ -1,9 +1,16 @@
 import type { FC } from "react";
 import { isAdmin } from "../../../lib/authorization";
 import { useAuthStore } from "../../../store/auth-store";
+import { LogOut } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const Header: FC = () => {
-  const { user } = useAuthStore();
+  const { user, logout } = useAuthStore();
+  const navigate = useNavigate();
+  function handleLogout() {
+    logout();
+    navigate("/login", { replace: true });
+  }
   return (
     <header className="h-16 flex items-center justify-between pr-6">
       <h1 className="text-2xl font-semibold">
@@ -15,6 +22,12 @@ const Header: FC = () => {
           alt="Avatar"
           className="w-9 h-9 rounded-full"
         />
+        <button
+          onClick={handleLogout}
+          className="bg-gray-400 hover:bg-gray-500 transition-all p-2 rounded-full cursor-pointer"
+        >
+          <LogOut />
+        </button>
       </div>
     </header>
   );
