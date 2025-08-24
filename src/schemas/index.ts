7fd81f73190f178a55fea9e4fd52a932ca412f13
Aiman Naeem <109482563+Aimancod01@users.noreply.github.com>
2025-08-25 00:22:12 +0500

    register api

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -2,8 +2,9 @@ import { z } from "zod";
 
 export const signUpSchema = z
   .object({
+    name: z.string().min(2, "Name must be at least 2 characters"),
     email: z.string().email("Enter a valid email"),
-    password: z.string().min(10, "Password must be at least 10 characters"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
     confirmPassword: z.string(),
     role: z.string(),
   })
